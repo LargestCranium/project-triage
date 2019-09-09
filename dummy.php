9e88481317011bffd68c05a62000d570c5a3ba50
HeccBoi <49526473+HeccBoi@users.noreply.github.com>
2019-09-09 12:55:06 -0400

    Created Dummy file
    Dummy file populated with dummy data

---
+++ dummy.php
@@ -0,0 +1,28 @@
+[
+  {
+  "patientGuid":"1",
+  "firstname":"Chunk",
+  "lastname":"Large",
+  "age":1,
+  "DOB": "1999-12-31"
+  "sexAtBirth":"M",
+  },
+
+  {
+  "patientGuid":"2",
+  "firstname":"Big",
+  "lastname":"Money",
+  "age":666,
+  "DOB": "2000-01-01"
+  "sexAtBirth":"F",
+  },
+
+  {
+  "patientGuid":"3",
+  "firstname":"Colonel",
+  "lastname":"Sanders",
+  "age":"?",
+  "DOB": "1900-12-31"
+  "sexAtBirth":"M",
+  }
+]
